ac1b27dc83ecfa0204779e7cbfa1a409b4b02975
KubiV <kubiwan007@gmail.com>
2024-07-13 21:55:06 +0200

    esp32 doc

--- Code/Tlak/Tlak.ino
+++ Code/Tlak/Tlak.ino
@@ -2,13 +2,59 @@
 #define OUT_PIN 2
 #define SCK_PIN 3
 
+// proměnné pro ukládání kalibračních dat
+float pressure1_mmHg = 40;
+float pressure2_mmHg = 200;
+long adcValue1 = 9252292;
+long adcValue2 = 10223993;
+bool averaging1 = false;
+bool averaging2 = false;
+long sum1 = 0;
+long sum2 = 0;
+int count1 = 0;
+int count2 = 0;
+
 void setup() {
-  pinMode(OUT_PIN, INPUT);   // Připoj modul HX710 OUT pin na Arduino např. pin D2
-  pinMode(SCK_PIN, OUTPUT);  // Připoj modul HX710 SCK pin na Arduino např. pin D3
+  pinMode(OUT_PIN, INPUT);   // připoj modul HX710 OUT pin na Arduino např. pin D2
+  pinMode(SCK_PIN, OUTPUT);  // připoj modul HX710 SCK pin na Arduino např. pin D3
   Serial.begin(BAUD_RATE);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+
+    if (command == "start40") {
+      averaging1 = true;
+      sum1 = 0;
+      count1 = 0;
+      Serial.println("Starting averaging for 40 mmHg...");
+    } else if (command == "stop40") {
+      averaging1 = false;
+      if (count1 > 0) {
+        adcValue1 = sum1 / count1;
+        Serial.print("Averaged ADC Value for 40 mmHg: ");
+        Serial.println(adcValue1);
+      } else {
+        Serial.println("No data to average for 40 mmHg.");
+      }
+    } else if (command == "start200") {
+      averaging2 = true;
+      sum2 = 0;
+      count2 = 0;
+      Serial.println("Starting averaging for 200 mmHg...");
+    } else if (command == "stop200") {
+      averaging2 = false;
+      if (count2 > 0) {
+        adcValue2 = sum2 / count2;
+        Serial.print("Averaged ADC Value for 200 mmHg: ");
+        Serial.println(adcValue2);
+      } else {
+        Serial.println("No data to average for 200 mmHg.");
+      }
+    }
+  }
+
   // čekání na konec čtení
   while (digitalRead(OUT_PIN)) {}
 
@@ -26,6 +72,19 @@ void loop() {
   // převěď na dvojkový doplněk - způsob řešení záporných čísel
   result = result ^ 0x800000;
 
+  // Průměrování hodnot pro kalibraci
+  if (averaging1) {
+    sum1 += result;
+    count1++;
+  }
+  if (averaging2) {
+    sum2 += result;
+    count2++;
+  }
+
+  // Vypočítej tlak v mmHg pomocí lineární interpolace mezi dvěma kalibračními body
+  float pressure_mmHg = mapCalibration(result, adcValue1, adcValue2, pressure1_mmHg, pressure2_mmHg);
+
   // 3x pulz na hodinový pin pro začátek měření
   for (char i = 0; i < 3; i++) {
     digitalWrite(SCK_PIN, HIGH);
@@ -33,5 +92,15 @@ void loop() {
   }
 
   // zobraz hodnotu
-  Serial.println(result);
+  Serial.print("Pressure: ");
+  Serial.print(pressure_mmHg);
+  Serial.println(" mmHg");
+
+  // pauza před dalším měřením
+  delay(10);
+}
+
+// funkce pro lineární mapování
+float mapCalibration(long x, long in1, long in2, float out1, float out2) {
+  return (float)(x - in1) * (out2 - out1) / (float)(in2 - in1) + out1;
 }
---
+++ Doc/ESP32/sketch_nov24a/sketch_nov24a.ino
@@ -0,0 +1,69 @@
+#define SAMPLE_RATE 125
+#define BAUD_RATE 115200
+
+void setup() {
+
+  Serial.begin(115200); // initialize the serial communication
+}
+ 
+void loop() {
+  
+  //Serial.print(analogRead(34)); // send the value of analog input 0
+
+// Calculate elapsed time
+  static unsigned long past = 0;
+  unsigned long present = micros();
+  unsigned long interval = present - past;
+  past = present;
+
+  // Run timer
+  static long timer = 0;
+  timer -= interval;
+
+  // Sample
+  if(timer < 0){
+    timer += 1000000 / SAMPLE_RATE;
+    float sensor_value = analogRead(34);
+    float signal = ECGFilter(sensor_value);
+    Serial.println(signal);
+    //Serial.print(0);
+    //Serial.print(";");
+    //Serial.println(analogRead(34));
+    //Serial.println(0);
+    delay(10);
+  }
+}
+
+float ECGFilter(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.70682283*z1 - 0.15621030*z2;
+    output = 0.28064917*x + 0.56129834*z1 + 0.28064917*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.95028224*z1 - 0.54073140*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.95360385*z1 - 0.95423412*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.98048558*z1 - 0.98111344*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  return output;
+}
